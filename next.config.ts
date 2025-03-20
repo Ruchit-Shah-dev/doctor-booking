ccf40eeef4062983d0d4fcda23f87259db57c7be
Ruchit <ruchit2502@gmail.com>
2025-03-20 10:19:37 +0530

    try to deploy on vercel attempt 2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Enables strict mode to catch errors early
   output: "standalone", // Helps in Vercel deployments by making the app independent of node_modules
-  distDir: "build", // Changes the default `.next/` directory to `build/` for better separation
   pageExtensions: ["tsx", "ts"], // Ensures Next.js correctly detects page files
   typescript: {
     ignoreBuildErrors: true,
